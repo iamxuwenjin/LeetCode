57c64bea26a752bc2eee08689ba145ac842e9874
wenjin.xu <18511551849@163.com>
2020-12-12 11:55:10 +0000

    test

--- code_list/两数之和.py
+++ code_list/两数之和.py
@@ -7,7 +7,7 @@ Site :
 File : 两数之和.py
 Software: PyCharm
 description：
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+给定一个整数数组 nums和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 """
 
@@ -22,5 +22,4 @@ class Solution:
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.twoSum(nums=[2,11,7,15], target=9))
-
+    print(solution.twoSum(nums=[2, 11, 7, 15], target=9))
---
+++ code_list/两数相加.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Time : 2020/8/21 6:02 下午
+Author : wenjin.xu
+Site :
+File : 两数之和.py
+Software: PyCharm
+description：
+给出两个非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+您可以假设除了数字 0 之外，这两个数都不会以 0开头。
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        pass
+
+
